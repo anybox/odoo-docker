832fb3b4f15a67e429bd9ed9058869f09b71e071
Christophe Combelles <ccomb@free.fr>
2021-08-31 10:52:07 +0200

    unaccent function

--- postgresql/init.sh
+++ postgresql/init.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 createuser -U postgres -d odoo
 createdb -U postgres -O odoo odoo
+
+# unaccent functions
+psql -c "CREATE SCHEMA unaccent_schema;" odoo odoo
+psql -c "CREATE EXTENSION unaccent WITH SCHEMA unaccent_schema;" odoo odoo
+psql -c "COMMENT ON EXTENSION unaccent IS 'text search dictionary that removes accents';" odoo odoo
+psql -c "CREATE FUNCTION public.unaccent(text) RETURNS text
+    LANGUAGE sql IMMUTABLE
+    AS \$_\$
+        SELECT unaccent_schema.unaccent('unaccent_schema.unaccent', \$1)
+        \$_\$;" odoo odoo
+
